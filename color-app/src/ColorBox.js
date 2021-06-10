35aab26109bee1c5338d7453817c1c4b5a70536d
elena-vost <angara1298@gmail.com>
2021-06-10 09:45:28 +0200

    add new style

--- color-app/src/ColorBox.js
+++ color-app/src/ColorBox.js
@@ -8,9 +8,11 @@ class ColorBox extends Component {
       <div style={{ background }} className="ColorBox">
         <div className="copy-container">
           <div className="box-content">
-            <span>{this.props.name}</span>
+            <span>{name}</span>
           </div>
+          <button className="copy-button">COPY</button>
         </div>
+        <span className="see-more">More</span>
       </div>
     );
   }

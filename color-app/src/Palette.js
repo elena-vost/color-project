5b0e2c8ee4db2422ee9960115e23bb6dacba2f0d
elena-vost <angara1298@gmail.com>
2021-06-10 09:21:26 +0200

    added paletts

--- color-app/src/Palette.js
+++ color-app/src/Palette.js
@@ -3,8 +3,8 @@ import React, { Component } from "react";
 export default class Palette extends Component {
   render() {
     return (
-      <div>
-        <h1>Pallets</h1>
+      <div className="Palette">
+        <div className="Palette-colors"></div>
       </div>
     );
   }

86dc8efda7f8cfbfff720c2ab9dc495ef1ab0fa7
elena-vost <angara1298@gmail.com>
2021-06-13 17:11:48 +0200

    slider styled mor4iidfe

--- color-app/src/Navbar.js
+++ color-app/src/Navbar.js
@@ -11,7 +11,7 @@ class Navbar extends Component {
     return (
       <header className="Navbar">
         <div className="logo">
-          <a href="#">reactcolorpicker</a>
+          <a href="#">Reactcolorpicker</a>
         </div>
         <div className="slider-container">
           <span>Level: {level}</span>

4310727aece66565752f98ff1901d034afe9342a
elena-vost <angara1298@gmail.com>
2021-06-14 15:12:22 +0200

    slider stydcsdsfsel

--- color-app/src/Navbar.js
+++ color-app/src/Navbar.js
@@ -11,7 +11,7 @@ class Navbar extends Component {
     return (
       <header className="Navbar">
         <div className="logo">
-          <a href="#">ReactcoloRPIcker</a>
+          <a href="#">ReactcolORPIcker</a>
         </div>
         <div className="slider-container">
           <span>Level: {level}</span>

8378241d12170b34a5796cea40b6fc7b33af464d
elena-vost <angara1298@gmail.com>
2021-06-16 11:36:46 +0200

    1606 2'

--- color-app/src/PaletteList.js
+++ color-app/src/PaletteList.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class PaletteList extends Component {
   render() {
+    const { palettes } = this.props();
     return (
       <div>
         <h1>REACTCOLORS</h1>
+        {palettes.map((palette) => (
+          <p>
+            <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
+          </p>
+        ))}
       </div>
     );
   }

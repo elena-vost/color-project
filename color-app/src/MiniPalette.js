0704f6eb0510c4dc2d75406199af1422c502b798
elena-vost <angara1298@gmail.com>
2021-06-17 08:52:31 +0200

    17062

--- color-app/src/MiniPalette.js
+++ color-app/src/MiniPalette.js
@@ -2,17 +2,34 @@ import React from "react";
 import { withStyles } from "@material-ui/styles";
 
 const styles = {
-  main: {
-    backgroundColor: "purple",
-    border: "3px solid teal",
+  root: {
+    backgroundColor: "white",
+    border: "1px solid black",
+    borderRadius: "5px",
+    padding: "0.5rem",
+    position: "relative",
+    overflow: "hidden",
+    "&:hover": {
+      cursor: "pointer",
+    },
   },
+  colors: {
+    backgroundColor: "grey",
+  },
+  title: {
+    display: "flex",
+  },
+  emoji: {},
 };
 function MiniPalette() {
-  const { classes } = props;
+  const { classes, paletteName, emoji } = props;
   return (
     <div className={classes.root}>
       <div className={classes.colors}></div>
-      <h5 className={classes.title}></h5>
+      <h5 className={classes.title}>
+        {paletteName}
+        <span className={classes.emoji}>{emoji}</span>
+      </h5>
     </div>
   );
 }

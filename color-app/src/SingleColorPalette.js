e840c3b39e488a38dd294374b09489ac33631b81
elena-vost <angara1298@gmail.com>
2021-06-21 09:29:21 +0200

    refactor Palette styles

--- color-app/src/SingleColorPalette.js
+++ color-app/src/SingleColorPalette.js
@@ -24,6 +24,26 @@ const styles = {
     cursor: "pointer",
     marginBottom: "-3.5px",
     opacity: 1,
+    backgroundColor: "black",
+    "& a": {
+      color: "white",
+      width: "100px",
+      height: "30px",
+      position: "absolute",
+      display: "inline-block",
+      top: "50%",
+      left: "50%",
+      marginLeft: "-50px",
+      marginTop: "-15px",
+      textAlign: "center",
+      outline: "none",
+      background: "rgba(255, 255, 255, 0.3)",
+      fontSize: "1rem",
+      lineHeight: "30px",
+      textTransform: "uppercase",
+      border: "none",
+      textDecoration: "none",
+    },
   },
 };
 
@@ -66,9 +86,7 @@ class SingleColorPalette extends Component {
         <div className={classes.colors}>
           {colorBoxes}
           <div className={classes.goBack}>
-            <Link to={`/palette/${id}`} className="back-button">
-              GO BACK
-            </Link>
+            <Link to={`/palette/${id}`}>GO BACK</Link>
           </div>
         </div>
         <PaletteFooter paletteName={paletteName} emoji={emoji} />
